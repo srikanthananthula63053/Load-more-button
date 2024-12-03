4c07e2eb5c66bd2fe5c9e8c75c5f4fdaab12c1f8
srikanthananthula63053 <sreekanthananthula5@gmail.com>
2024-12-03 11:10:06 +0530

    creating useEffect for adding fetch

---
+++ src/components/load-more-product/index.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export default function LoadMoreButton() {
+
+    const [loading, setLoading] = useState(false);
+    const [product, setProduct] = useState([]);
+    const [count, setCount] = useState(0);
+
+
+    return (
+        <div>
+
+        </div>
+    );
+}
